b73441fbe86a4761e96f8ff6a7daf09c457915e1
John Hunter <john@johnhunter.info>
2023-07-18 11:07:52 +0100

    Add some scratch examples

---
+++ src/scratch.ts
@@ -0,0 +1,62 @@
+/// optional chaining and nullish coalescing
+
+let x: {
+  name: string;
+  user: {
+    address?: {
+      street: string;
+      city: string;
+    };
+  };
+} = undefined as any;
+
+console.log(x.user.address?.city);
+
+class Foo {
+  #name;
+  constructor(name?: string) {
+    this.#name = name ?? '(no name)';
+  }
+}
+
+/// tuple types
+
+/// variadic tuple types
+type Bar<T extends any[]> = [boolean, ...T, boolean];
+
+/// labels
+type Address = [
+  streetNumber: number,
+  city: string,
+  state: string,
+  postal: number,
+];
+function printAddress(...address: Address) {}
+
+// @ts-expect-error
+const num1: number = true;
+
+// @ts-ignore
+const num2: number = true;
+
+// type guard
+function isError(err: any): err is Error {
+  return err instanceof Error;
+}
+
+// can assert in tests (not in app code)
+function assertIsError(err: any): asserts err is Error {
+  if (isError(err)) throw new Error(`Not an error: ${err}`);
+}
+
+// typing try/catch
+function somethingRisky() {}
+try {
+  somethingRisky();
+} catch (err: unknown) {
+  if (isError(err)) {
+    console.log(err.stack);
+  } else {
+    console.log(err);
+  }
+}
