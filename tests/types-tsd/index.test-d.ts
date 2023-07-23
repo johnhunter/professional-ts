88f7a4d872cc82aa73509988fc92e454e571abe1
John Hunter <john@johnhunter.info>
2023-07-23 14:36:21 +0100

    Add tests for data types

--- tests/types-tsd/index.test-d.ts
+++ tests/types-tsd/index.test-d.ts
@@ -1,5 +1,24 @@
-import { ITeam } from '../../src/types';
+import { ITeam, IUser, IMessage, IChannel } from '../../src/types';
 import { expectNotAssignable, expectAssignable } from 'tsd';
+
+expectNotAssignable<IUser>(null);
+expectAssignable<IUser>({
+  id: 1,
+  username: '',
+  name: '',
+  iconUrl: '',
+});
+
+expectNotAssignable<IMessage>(null);
+expectAssignable<IMessage>({
+  id: 1,
+  teamId: '',
+  channelId: '',
+  userId: '',
+  createdAt: '',
+  user: {} as IUser,
+  body: '',
+});
 
 expectNotAssignable<ITeam>(null);
 expectAssignable<ITeam>({
@@ -7,4 +26,13 @@ expectAssignable<ITeam>({
   iconUrl: '',
   id: '',
   name: '',
+});
+
+expectNotAssignable<IChannel>(null);
+expectAssignable<IChannel>({
+  id: '',
+  name: '',
+  description: '',
+  teamId: '',
+  messages: [{} as IMessage],
 });
